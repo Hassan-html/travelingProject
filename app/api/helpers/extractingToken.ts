061e523768d69ee5abf084cc60e474ea8a7a3c07
HSN-CODES <logics.nook@gmail.com>
2024-03-29 11:37:50 +0500

    cookie

--- app/api/helpers/extractingToken.ts
+++ app/api/helpers/extractingToken.ts
@@ -3,8 +3,8 @@ import { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
 
 export const extractingData = async (request: NextRequest) => {
+  const token = request.cookies.get("token")?.value || "";
   try {
-    const token = request.cookies.get("token")?.value || "";
     const decodedData: any = jwt.verify(token, process.env.TOKEN_SECRET!);
     return decodedData.id;
   } catch (error) {
