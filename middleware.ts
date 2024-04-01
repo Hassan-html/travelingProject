b58fe35b58bc6817e276837056fb164e32206998
HSN-CODES <logics.nook@gmail.com>
2024-04-01 06:54:12 +0500

    bug

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const checkingCookie = request.cookies.get("token") || "";
+  const checkingCookie = request.cookies.get("token")?.value || "";
   const url = request.nextUrl.pathname;
   if ((url.includes("/login") || url.includes("/register")) && checkingCookie) {
     return NextResponse.rewrite(new URL("/", request.url));
